f2aea191db1b638d66908cfcc5d9bd7a23c6924e
Anna Sours <acsours@gmail.com>
2021-03-22 17:19:10 -0600

    handle input field started

--- static/js/app.js
+++ static/js/app.js
@@ -27,7 +27,61 @@ var tbody = d3.select("tbody");
 // newRow.append('td').text('2 minutes');
 // newRow.append('td').text('4 bright green circles high in the sky going in circles then one bright green light at my front door.');
 
-// this shows the entire table as is --> we will use this to 
+// get reference to the button on the page
+var button = d3.select("#filter-btn")
+
+// get reference to the input element
+var inputField = d3.select(".form-control")
+
+
+// testing button
+
+function handleClick() {
+    console.log("A button was clicked!");
+  
+    // We can use d3 to see the object that dispatched the event
+    console.log(d3.event.target);
+  }
+  
+  // We can use the `on` function in d3 to attach an event to the handler function
+button.on("click", handleClick);
+
+// function handleInput() {
+//     var inputText=d3.event.target.value;
+//     console.log(inputText);
+// };
+
+// inputField.on("change", function() {
+//     var inputText=d3.event.target.value;
+//     console.log(inputText);
+// });
+
+
+
+function handleChange(event) {
+    var inputText=d3.event.target.value;
+    console.log(inputText);    
+    
+    for (var i=0; i<tableData.length; i++) {
+        var new_row=tbody.append('tr');
+
+        current_data=tableData[i]
+
+        value_list = Object.values(current_data);
+        // console.log("value_list: ");
+        // console.log(value_list)
+        if (value_list[0]===inputText) {
+            for (var j=0; j<value_list.length; j++) {
+                new_row.append('td').text(value_list[j]);
+            };
+        };
+    };
+};
+
+
+inputField.on("change", handleChange)
+// // ***********************************
+  // this shows the entire table as is --> we will use this to 
 // fill a table based on data that has been filtered
 // for (var i=0; i<tableData.length; i++) {
 //     var new_row=tbody.append('tr');
@@ -37,27 +91,29 @@ var tbody = d3.select("tbody");
 //     value_list = Object.values(current_data);
 //     // console.log("value_list: ");
 //     // console.log(value_list)
-//     if (value_list[0]==="1/1/2010") {
+//     if (value_list[0]===inputText) {
 //         for (var j=0; j<value_list.length; j++) {
 //             new_row.append('td').text(value_list[j]);
 //         };
 //     };
 // };
+// // ***********************************
+// // ***********************************
+// // If you want to account for disorganized data, do it this way instead:
+// var header_list=['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments']
 
 
-// If you want to account for disorganized data, do it this way instead:
-var header_list=['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments']
+// for (var i=0; i<tableData.length; i++) {
+//     var new_row=tbody.append('tr');
 
+//     current_data=tableData[i]
+//     for (var j=0; j<header_list.length; j++) {
+//             new_row.append('td').text(current_data[header_list[j]]);
+//         // console.log(current_data[header_list[j]])
+//     };
+// };
+// // ***********************************
 
-for (var i=0; i<tableData.length; i++) {
-    var new_row=tbody.append('tr');
-
-    current_data=tableData[i]
-    for (var j=0; j<header_list.length; j++) {
-            new_row.append('td').text(current_data[header_list[j]]);
-        // console.log(current_data[header_list[j]])
-    };
-};
 
 // for (var i=0; i<tableData.length; i++) {
 //     var new_row=tbody.append('tr');
