8673dfcf8d3c8dd7c0090785ee408527ad86ad81
Anna Sours <acsours@gmail.com>
2021-03-27 12:33:03 -0600

    testing bonus strategies

--- static/js/app.js
+++ static/js/app.js
@@ -16,25 +16,28 @@ var tbody = d3.select("tbody");
 // I can create header_list in a simpler way:
 var header_list=Object.keys(tableData[0]);
 
+function buildTable(data) {
+  for (var i=0; i<tableData.length; i++) {
+      var new_row=tbody.append('tr');
 
-for (var i=0; i<tableData.length; i++) {
-    var new_row=tbody.append('tr');
-
-    current_data=tableData[i]
-    for (var j=0; j<header_list.length; j++) {
-            new_row.append('td').text(current_data[header_list[j]]);
-    };
+      current_data=tableData[i]
+      for (var j=0; j<header_list.length; j++) {
+              new_row.append('td').text(current_data[header_list[j]]);
+      };
+  };
 };
+
+buildTable(tableData)
 
 // get reference to the input fields on the page
 
 var inputField=d3.selectAll(".filter")
-
+// var form=d3.select("form")
 
   // We can use the `on` function in d3 to attach an event to the handler function
 inputField.on("change", handleChange);
 
-
+// form.on("change", handleChange);
 
 function handleChange(event) {
 
@@ -58,11 +61,12 @@ function handleChange(event) {
 
   // console.log("id= " + inputID)
   // console.log("filterValue =" + inputText)
-
+  if (inputText) {
   // then I want to cycle through the sightings and check if id matches, then filter by the value of that id
-  filteredData=tableData.filter(sighting=>sighting[inputID]===inputText)
+  var filteredData=tableData.filter(sighting=>sighting[inputID]===inputText)
     // console.log(filteredData);   
-    
+  }
+
   filteredData.forEach(record=> {
     var new_row=tbody.append('tr');
     value_list=Object.values(record)
